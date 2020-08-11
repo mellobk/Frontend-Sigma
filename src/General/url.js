032682f5ca420ac1108affd6908f26dcf0713d77
brayan alexander manrique bonilla <brayanalexandermanriquebonilla@hotmail.com>
2020-08-11 15:08:02 -0500

    commit modificaciones de variable de entorno issiu

--- src/General/url.js
+++ src/General/url.js
@@ -5,7 +5,7 @@ let URL
 if(process.env.NODE_ENV==='development'){
      URL='http://localhost:8000/'
 }else{
-    URL='https://xtremestudio.com.co/Api-sigma'
+    URL='https://xtremestudio.com.co/Api-sigma/'
 }
 //export const URL='https://xtremestudio.com.co/API/'
 export default URL
